542820f5b7af54ea4d92e947e7c4b37bed032ca6
Statdk <morgirme@gmail.com>
2024-02-07 15:04:49 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const path = require("path");
+
+function startWindow() {
+  const window = new electron.BrowserWindow({
+    width: 1200,
+    height: 800,
+  });
+
+  window.setMenu(null);
+  window.loadFile(path.join(__dirname, "src", "index.html"));
+}
+
+electron.app.whenReady().then(() => {
+  startWindow();
+});
+
+electron.app.on("window-all-closed", () => {
+  electron.app.quit();
+});
