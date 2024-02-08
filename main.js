1f3fccd3018733abc55aaf39e1fd27ac4db3a6d4
Statdk <morgirme@gmail.com>
2024-02-07 16:05:41 -0800

    Font Setup

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ function startWindow() {
   const window = new electron.BrowserWindow({
     width: 1200,
     height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "src", "preload.js"),
+    },
   });
 
-  window.setMenu(null);
+  // window.setMenu(null);
   window.loadFile(path.join(__dirname, "src", "index.html"));
 }
 
